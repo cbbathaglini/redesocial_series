1f2942bcd6f7aca17226254ff1b723f868df7403
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2023-08-22 14:25:49 -0300

    adicionando lógica do banco

---
+++ src/main/java/br/com/entretenimento/config/Database.java
@@ -0,0 +1,28 @@
+package br.com.entretenimento.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Database {
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    public Database(){
+
+    }
+
+    public void  open_connection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        this.connection =  DriverManager.getConnection(DatabaseConfigs.URL, DatabaseConfigs.USERNAME, DatabaseConfigs.PASSWORD);
+
+    }
+
+    public void  close_connection() throws SQLException {
+        this.connection.close();
+    }
+
+    public Connection getConnection(){
+        return this.connection;
+    }
+}
---
+++ src/main/java/br/com/entretenimento/config/DatabaseConfigs.java
@@ -0,0 +1,7 @@
+package br.com.entretenimento.config;
+
+public class DatabaseConfigs {
+    protected static final String URL = "jdbc:mysql://localhost:3306/redesocial_series";
+    protected static final String USERNAME = "root";
+    protected static final String PASSWORD = "";
+}
