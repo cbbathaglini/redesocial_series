76ace0b56e1b005d37699aadfc683cf7fce83d9d
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2023-08-22 20:22:53 -0300

    changing database

--- database.sql
+++ database.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE redesocial_series;
 use redesocial_series;
+
 CREATE TABLE series(
 idSerie int not null AUTO_INCREMENT,
 nome varchar(200) not null,
@@ -27,28 +28,29 @@ INSERT INTO usuarios (nome, email) VALUES ("usuario-dois","usuario-dois@email.co
 CREATE TABLE notas_series(
  idNotasSeries int not null AUTO_INCREMENT,
  idSerie int not null,
+ idUsuario int not null,
  nota int DEFAULT 1 not null,
  comentarios varchar(400) null,
  PRIMARY KEY(idNotasSeries),
- FOREIGN KEY (idSerie) REFERENCES series(idSerie),
+ FOREIGN KEY (idSerie) REFERENCES series(idSerie)
 );
 
-INSERT INTO notas_series (idSerie,nota,comentarios) VALUES (6,4,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
-INSERT INTO notas_series (idSerie,nota,comentarios) VALUES (6,5,"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-INSERT INTO notas_series (idSerie,nota,comentarios) VALUES (1,4,"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
+INSERT INTO notas_series (idSerie,idUsuario,nota,comentarios) VALUES (6,4,2,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+INSERT INTO notas_series (idSerie,idUsuario,nota,comentarios) VALUES (6,5,2,"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+INSERT INTO notas_series (idSerie,idUsuario, nota,comentarios) VALUES (1,4,2,"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
 
 CREATE TABLE minhas_series(
   idMinhasSeries int not null AUTO_INCREMENT,
   idUsuario int not null,
   idSerie int not null,
   situacao varchar(200) not null,
-  favorita char(1)  null,
+  favorita boolean  null,
   PRIMARY KEY(idMinhasSeries),
   FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario),
   FOREIGN KEY (idSerie) REFERENCES series(idSerie)
 );
 CREATE INDEX idx_id_usuario ON minhas_series (idUsuario);
 
-INSERT INTO minhas_series (idUsuario,idSerie,situacao,favorita) VALUES (2,4,"JA VI","S");
+INSERT INTO minhas_series (idUsuario,idSerie,situacao,favorita) VALUES (2,4,"JA VI",TRUE);
 INSERT INTO minhas_series (idUsuario,idSerie,situacao,favorita) VALUES (2,5,"QUERO VER",null);
-INSERT INTO minhas_series (idUsuario,idSerie,situacao,favorita) VALUES (2,6,"ESTOU VENDO","S");
+INSERT INTO minhas_series (idUsuario,idSerie,situacao,favorita) VALUES (2,6,"ESTOU VENDO",TRUE);
