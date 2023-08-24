6afb1700c4f84cd26663bc67472800eb9fe85308
Carine Bertagnolli Bathaglini <cbbathaglini@gmail.com>
2023-08-23 23:29:43 -0300

    Update Database.java

--- src/main/java/br/com/series/config/Database.java
+++ src/main/java/br/com/series/config/Database.java
@@ -6,10 +6,21 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Database {
+    private static Database databaseInstance;
     private Connection connection;
     private PreparedStatement preparedStatement;
+    private Statement statement;
+
     public Database(){
 
+    }
+
+    //Singleton
+    public static Database getInstance(){
+        if(databaseInstance == null){
+            databaseInstance = new Database();
+        }
+        return databaseInstance;
     }
 
     public void  open_connection() throws SQLException, ClassNotFoundException {
@@ -24,5 +35,22 @@ public class Database {
 
     public Connection getConnection(){
         return this.connection;
+    }
+
+
+    public PreparedStatement  executarSQL(String query,Integer statement) throws SQLException {
+        if(statement!= null) {
+            this.preparedStatement = this.connection.prepareStatement(query, statement);
+        }else{
+            this.preparedStatement = this.connection.prepareStatement(query);
+        }
+        return this.preparedStatement;
+
+    }
+
+    public ResultSet  consultarSQL(String query) throws SQLException {
+        this.statement  = this.connection.createStatement();
+        return this.statement.executeQuery(query);
+
     }
 }
